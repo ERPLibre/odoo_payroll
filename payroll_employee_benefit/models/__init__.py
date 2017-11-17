56993a4f2da8b843a923cbb2ee01907b1f19bebf
PPTS\kaja.mydeen <kajamydeen.i@pptservices.com>
2017-11-17 12:25:01 +0530

    Benefit module converted to odoo10

--- payroll_employee_benefit/models/__init__.py
+++ payroll_employee_benefit/models/__init__.py
@@ -18,11 +18,11 @@
 #
 ##############################################################################
 
-from . import hr_contract
-from . import hr_employee_benefit_category
-from . import hr_employee_benefit
-from . import hr_employee_benefit_rate
-from . import hr_employee_benefit_rate_line
-from . import hr_payslip
-from . import hr_payslip_benefit_line
-from . import hr_salary_rule
+import hr_contract
+import hr_employee_benefit_category
+import hr_employee_benefit
+import hr_employee_benefit_rate
+import hr_employee_benefit_rate_line
+import hr_payslip
+import hr_payslip_benefit_line
+import hr_salary_rule
