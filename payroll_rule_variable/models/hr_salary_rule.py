8b8f5425735e0adff96d694710bea8c921db0fd8
Mathieu Benoit <mathben@technolibre.ca>
2020-09-25 03:08:05 -0400

    [MIG] ./payroll_rule_variable: Migration to 12.0

--- payroll_rule_variable/models/hr_salary_rule.py
+++ payroll_rule_variable/models/hr_salary_rule.py
@@ -18,9 +18,9 @@
 #
 ##############################################################################
 
-from openerp import api, fields, models, _
-from openerp.exceptions import ValidationError
-from openerp.tools.safe_eval import safe_eval as eval
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+from odoo.tools.safe_eval import safe_eval as eval
 
 
 class HrSalaryRule(models.Model):
